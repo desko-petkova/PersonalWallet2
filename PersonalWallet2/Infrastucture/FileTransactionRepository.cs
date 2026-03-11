188855e318b31e41cf510b08a4a6d64acd32d991
PCK216 <PCK216@K216>
2026-03-11 10:33:25 +0200

    GetByAccountId Save methods in FileAccountRepository implemented

---
+++ PersonalWallet2/Infrastucture/FileTransactionRepository.cs
@@ -0,0 +1,44 @@
+﻿using PersonalWallet2.Domain.Entities;
+using PersonalWallet2.Domain.ValueObjects;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PersonalWallet2.Infrastucture
+{
+    public class FileTransactionRepository
+    {
+        private readonly FileStorage _storage;
+
+        public FileTransactionRepository(FileStorage storage)
+        {
+            _storage = storage;
+        }
+        public void Save(Transaction transaction)
+        {
+            var db = _storage.Load();
+            
+            var newTransaction = new Transaction(
+                db.NextId++,
+                transaction.AccountId,
+                transaction.Type,
+                transaction.Amount,
+                transaction.Date
+                );
+            db.Transactions.Add( newTransaction );
+
+            _storage.Save(db);
+        }
+        public IReadOnlyList<Transaction> GetByAccountId(int acountId)
+        {
+            var db = _storage.Load();
+
+            List<Transaction> result = new List<Transaction>();
+
+            foreach (Transaction transaction in db.Transactions)
+            {
+                if( transaction.AccountId == acountId )
+                result.Add( transaction );
+            }
+            return result;
+        }
+    }
+}
