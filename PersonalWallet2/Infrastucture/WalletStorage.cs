362cafc76129f47bf63828188a3f8e5192a3b35c
PCK216 <PCK216@K216>
2026-03-11 10:32:10 +0200

    NextId proparty added in WalletStorage

--- PersonalWallet2/Infrastucture/WalletStorage.cs
+++ PersonalWallet2/Infrastucture/WalletStorage.cs
@@ -4,7 +4,7 @@ namespace PersonalWallet2.Infrastucture
 {
     public class WalletStorage
     {
-
+        public int NextId { get; set; } = 1;
         public List<Account> Accounts { get; set; } = new List<Account>();
 
         public List<Transaction> Transactions { get; set; } = new List<Transaction>();
