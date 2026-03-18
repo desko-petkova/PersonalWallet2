dc1df645bb10a07b6e4cdb5e64bc3c6d8e549e0d
PCK216 <PCK216@K216>
2026-03-18 08:43:03 +0200

    ITransactionRepository interface implemented

--- PersonalWallet2/Application/Interfaces/ITransactionRepository.cs
+++ PersonalWallet2/Application/Interfaces/ITransactionRepository.cs
@@ -1,7 +1,11 @@
-﻿namespace PersonalWallet2.Application.Interfaces
+﻿using PersonalWallet2.Domain.Entities;
+
+namespace PersonalWallet2.Application.Interfaces
 {
     public interface ITransactionRepository
     {
+        void Save(Transaction transaction);
+        IReadOnlyList<Transaction> GetByAccountId(int acountId);
 
     }
 }
