41c71db150ad933c4cd211022f48ce80e94f599e
PCK216 <PCK216@K216>
2026-03-18 10:51:02 +0200

    Program configuration added

--- PersonalWallet2/Program.cs
+++ PersonalWallet2/Program.cs
@@ -1,4 +1,7 @@
-﻿using PersonalWallet2.Infrastucture;
+﻿using PersonalWallet2.Application;
+using PersonalWallet2.Application.Interfaces;
+using PersonalWallet2.ConsoleUI;
+using PersonalWallet2.Infrastucture;
 namespace PersonalWallet2
 {
     internal class Program
@@ -6,6 +9,16 @@ namespace PersonalWallet2
         static void Main(string[] args)
         {
             var storage = new FileStorage("wallet.json");
+
+            IAccountRepository accountRepo = new FileAccountRepository(storage);
+
+            ITransactionRepository transactionRepo = new FileTransactionRepository(storage);
+
+            var service = new WalletService(accountRepo, transactionRepo);
+
+            var ui = new WalletConsoleUI(service);
+
+            ui.Run();
         }
     }
 }
