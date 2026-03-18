f96766689780c6d4e8e90cefd3dc6c76fb78bdc5
PCK216 <PCK216@K216>
2026-03-18 10:51:27 +0200

    Using story added

--- PersonalWallet2/Application/WalletService.cs
+++ PersonalWallet2/Application/WalletService.cs
@@ -1,13 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PersonalWallet2.Application.Interfaces;
+using PersonalWallet2.Domain.Entities;
+using PersonalWallet2.Domain.Enums;
+using PersonalWallet2.Domain.ValueObjects;
 
 namespace PersonalWallet2.Application
 {
     public class WalletService
     {
+        private readonly IAccountRepository accountRepo;
+        private readonly ITransactionRepository transactionRepo;
+
+        public WalletService(IAccountRepository accountRepo, ITransactionRepository transactionRepo)
+        {
+            this.accountRepo = accountRepo;
+            this.transactionRepo = transactionRepo;
+        }
+
+        public void CreateAccount(string name, AccountType type, decimal amount)
+        {
+            var account = new Account(
+                0,
+                name,
+                type,
+                new Money(amount)
+                );
+            accountRepo.Save(account);
+        }
+
+        public IReadOnlyList<Account> GetAllAccounts()
+        {
+            return accountRepo.GetAll();
+        }
 
     }
 }
---
+++ PersonalWallet2/ConsoleUI/WalletConsoleUI.cs
@@ -0,0 +1,99 @@
+﻿using PersonalWallet2.Application;
+using PersonalWallet2.Domain.Enums;
+using System.Collections.Concurrent;
+
+namespace PersonalWallet2.ConsoleUI
+{
+    public class WalletConsoleUI
+    {
+        private readonly WalletService service;
+        public WalletConsoleUI(WalletService service)
+        {
+            this.service = service;
+        }
+
+        public void Run()
+        {
+            bool running = true;
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("======== Wallet System ==========");
+                Console.WriteLine("1. Create account");
+                Console.WriteLine("2. Add income");
+                Console.WriteLine("3. Add expense");
+                Console.WriteLine("4. Show accounts");
+                Console.WriteLine("5. Show transactions");
+                Console.WriteLine("0. Exit");
+
+                Console.WriteLine("Choose: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        CreateAcount();
+                        break;
+                    case "2":
+                        AddTransaction(TransactionType.Income);
+                        break;
+                    case "3":
+                        AddTransaction(TransactionType.Expense);
+                        break;
+                    case "4":
+                        ShowAccount();
+                        break;
+                    case "5":
+                        ShowTransaction();
+                        break;
+                    case "0":
+                        running = false;
+                        break;
+                }
+            }
+
+        }
+
+        private void ShowTransaction()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddTransaction(TransactionType income)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateAcount()
+        {
+            Console.Write("Enter account name:");
+            string name = Console.ReadLine();
+
+
+
+            Console.Write("Amount: ");
+            string amountInput = Console.ReadLine();
+
+            if (!decimal.TryParse(amountInput, out decimal amount))
+            {
+                Console.WriteLine("Invalid input!");
+                return;
+            }
+
+
+        //Cash,
+        //Bank,
+        //DebitCart,
+        //VitualCart,
+        //SavingBank
+        }
+    }
+}
+
+        
+    
